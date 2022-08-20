760d36568cb382896b335056754652fbfc86418f
Nicolas Moreira Nobre Leite <54647103+nicolasmnl@users.noreply.github.com>
2022-08-20 18:27:42 -0300

    Update fork-sleep-join

--- ex3/fork-sleep-join.go
+++ ex3/fork-sleep-join.go
@@ -6,9 +6,11 @@ import (
 	"time"
 )
 
-func sleepRoutines(joinChan chan int) {
-	time.Sleep(time.Second * time.Duration(rand.Intn(5)))
-	fmt.Printf("GoRoutine terminou\n")
+func sleepRoutines(joinChan chan int, my_id int) {
+	v := rand.Intn(5)
+	fmt.Printf("ID: [%d] Vou dormir por %d segundos\n", my_id, v)
+	time.Sleep(time.Second * time.Duration(v))
+	fmt.Printf("ID: [%d] Acordei :)\n", my_id)
 	joinChan <- 1
 }
 
@@ -18,7 +20,7 @@ func main() {
 	joinCh := make(chan int)
 
 	for i := 0; i < n; i++ {
-		go sleepRoutines(joinCh)
+		go sleepRoutines(joinCh, i)
 	}
 
 	for i := 0; i < n; i++ {
